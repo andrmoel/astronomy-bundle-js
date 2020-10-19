03f8a5462c2793c625b4c3bce68477dc4d2ca74e
github version update <noreply@github.com>
2020-10-19 17:36:13 +0000

    chore(release): 2.1.1

---
+++ lib/utils/angle.d.ts
@@ -0,0 +1,3 @@
+export declare function deg2rad(degrees: number): number;
+export declare function rad2deg(radians: number): number;
+export declare function normalizeAngle(degrees: number, baseAngle?: number): number;
