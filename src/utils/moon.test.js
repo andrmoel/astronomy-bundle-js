eaa97039f84def1faa8605b73c6dadf54435fd5f
Andreas Moeller <andrmoel@gmail.com>
2020-10-19 23:17:40 +0200

    feat(moon): add moon calculations

--- src/utils/moon.test.js
+++ src/utils/moon.test.js
@@ -1,4 +1,14 @@
-import {getArgumentOfLatitude, getMeanAnomaly, getMeanElongation} from './moon';
+import {
+    getApparentLongitude,
+    getArgumentOfLatitude,
+    getDistanceToEarth,
+    getEquatorialHorizontalParallax,
+    getLatitude,
+    getLongitude,
+    getMeanAnomaly,
+    getMeanElongation,
+    getMeanLongitude,
+} from './moon';
 import {round} from './math';
 
 it('tests getMeanElongation', () => {
@@ -17,4 +27,40 @@ it('tests getArgumentOfLatitude', () => {
     const T = -0.077221081451;
 
     expect(round(getArgumentOfLatitude(T), 6)).toBe(219.889721);
+});
+
+it('tests getMeanLongitude', () => {
+    const T = -0.077221081451;
+
+    expect(round(getMeanLongitude(T), 6)).toBe(134.290182);
+});
+
+it('tests getEquatorialHorizontalParallax', () => {
+    const T = -0.077221081451;
+
+    expect(round(getEquatorialHorizontalParallax(T), 6)).toBe(0.991990);
+});
+
+it('tests getDistanceToEarth', () => {
+    const T = -0.077221081451;
+
+    expect(round(getDistanceToEarth(T), 1)).toBe(368409.7);
+});
+
+it('tests getLongitude', () => {
+    const T = -0.077221081451;
+
+    expect(round(getLongitude(T), 6)).toBe(133.162655);
+});
+
+it('tests getApparentLongitude', () => {
+    const T = -0.077221081451;
+
+    expect(round(getApparentLongitude(T), 6)).toBe(133.167265);
+});
+
+it('tests getLatitude', () => {
+    const T = -0.077221081451;
+
+    expect(round(getLatitude(T), 6)).toBe(-3.229126);
 });
--- src/utils/moon.ts
+++ src/utils/moon.ts
@@ -1,4 +1,7 @@
-import {normalizeAngle} from './angle';
+import {MOON_ARGUMENTS_B, MOON_ARGUMENTS_LR} from '../constants/moon';
+import {deg2rad, normalizeAngle, rad2deg} from './angle';
+import {getMeanAnomaly as getMeanSunAnomaly} from './sun';
+import {getNutationInLongitude} from './earth';
 
 export function getMeanElongation(T: number): number {
     // Meeus 47.2
@@ -31,4 +34,185 @@ export function getArgumentOfLatitude(T: number): number {
         + Math.pow(T, 4) / 86331000;
 
     return normalizeAngle(F);
+}
+
+export function getMeanLongitude(T: number): number {
+    // Meeus 47.1
+    const L = 218.3164477
+        + 481267.88123421 * T
+        - 0.0015786 * Math.pow(T, 2)
+        + Math.pow(T, 3) / 538841
+        - Math.pow(T, 4) / 65194000;
+
+    return normalizeAngle(L);
+}
+
+export function getEquatorialHorizontalParallax(T: number): number {
+    const d = getDistanceToEarth(T);
+
+    // Meeus 47
+    return rad2deg(Math.asin(6378.14 / d));
+}
+
+export function getDistanceToEarth(T: number): number {
+    const sumR = _getSumR(T);
+
+    return 385000.56 + (sumR / 1000);
+}
+
+export function getLongitude(T: number): number {
+    const L = getMeanLongitude(T);
+    const sumL = _getSumL(T);
+
+    return L + (sumL / 1000000);
+}
+
+export function getApparentLongitude(T: number): number {
+    const l = getLongitude(T);
+    const phi = getNutationInLongitude(T);
+
+    return l + phi;
+}
+
+export function getLatitude(T: number): number {
+    const sumB = _getSumB(T);
+
+    return sumB / 1000000;
+}
+
+function _getSumR(T: number): number {
+    // Meeus 47.b
+    const D = getMeanElongation(T);
+    const Msun = getMeanSunAnomaly(T);
+    const Mmoon = getMeanAnomaly(T);
+    const F = getArgumentOfLatitude(T);
+
+    // Action of jupiter
+    const E = 1 - 0.002516 * T - 0.0000074 * Math.pow(T, 2);
+
+    let sumR = 0;
+    MOON_ARGUMENTS_LR.forEach((args: Array<number>) => {
+        const argD = args[0];
+        const argMsun = args[1];
+        const argMmoon = args[2];
+        const argF = args[3];
+        const argSumR = args[5];
+
+        let tmpSumR = Math.cos(deg2rad(argD * D + argMsun * Msun + argMmoon * Mmoon + argF * F));
+
+        switch (argMsun) {
+            case 1:
+            case -1:
+                tmpSumR = tmpSumR * argSumR * E;
+                break;
+            case 2:
+            case -2:
+                tmpSumR = tmpSumR * argSumR * E * E;
+                break;
+            default:
+                tmpSumR = tmpSumR * argSumR;
+                break;
+        }
+
+        sumR += tmpSumR;
+    });
+
+    return sumR;
+}
+
+function _getSumL(T: number): number {
+    // Meeus 47.b
+    const L = getMeanLongitude(T);
+    const D = getMeanElongation(T);
+    const Msun = getMeanSunAnomaly(T);
+    const Mmoon = getMeanAnomaly(T);
+    const F = getArgumentOfLatitude(T);
+
+    // Action of venus
+    const A1 = 119.75 + 131.849 * T;
+    // Action of jupiter
+    const A2 = 53.09 + 479264.290 * T;
+    const E = 1 - 0.002516 * T - 0.0000074 * Math.pow(T, 2);
+
+    let sumL = 3958 * Math.sin(deg2rad(A1))
+        + 1962 * Math.sin(deg2rad(L - F))
+        + 318 * Math.sin(deg2rad(A2));
+
+    MOON_ARGUMENTS_LR.forEach((args: Array<number>) => {
+        const argD = args[0];
+        const argMsun = args[1];
+        const argMmoon = args[2];
+        const argF = args[3];
+        const argSumL = args[4];
+
+        let tmpSumL = Math.sin(deg2rad(argD * D + argMsun * Msun + argMmoon * Mmoon + argF * F));
+
+        switch (argMsun) {
+            case 1:
+            case -1:
+                tmpSumL = tmpSumL * argSumL * E;
+                break;
+            case 2:
+            case -2:
+                tmpSumL = tmpSumL * argSumL * E * E;
+                break;
+            default:
+                tmpSumL = tmpSumL * argSumL;
+                break;
+        }
+
+        sumL += tmpSumL;
+    });
+
+    return sumL;
+}
+
+function _getSumB(T: number): number {
+    // Meeus 47.B
+    const L = getMeanLongitude(T);
+    const D = getMeanElongation(T);
+    const Msun = getMeanSunAnomaly(T);
+    const Mmoon = getMeanAnomaly(T);
+    const F = getArgumentOfLatitude(T);
+
+    // Action of venus
+    const A1 = 119.75 + 131.849 * T;
+    // Action of jupiter
+    const A3 = 313.45 + 481266.484 * T;
+    const E = 1 - 0.002516 * T - 0.0000074 * Math.pow(T, 2);
+
+    let sumB = -2235 * Math.sin(deg2rad(L))
+        + 382 * Math.sin(deg2rad(A3))
+        + 175 * Math.sin(deg2rad(A1 - F))
+        + 175 * Math.sin(deg2rad(A1 + F))
+        + 127 * Math.sin(deg2rad(L - Mmoon))
+        - 115 * Math.sin(deg2rad(L + Mmoon));
+
+    MOON_ARGUMENTS_B.forEach((args: Array<number>) => {
+        const argD = args[0];
+        const argMsun = args[1];
+        const argMmoon = args[2];
+        const argF = args[3];
+        const argSumB = args[4];
+
+        let tmpSumB = Math.sin(deg2rad(argD * D + argMsun * Msun + argMmoon * Mmoon + argF * F));
+
+        switch (argMsun) {
+            case 1:
+            case -1:
+                tmpSumB = tmpSumB * argSumB * E;
+                break;
+            case 2:
+            case -2:
+                tmpSumB = tmpSumB * argSumB * Math.pow(E, 2);
+                break;
+            default:
+                tmpSumB = tmpSumB * argSumB;
+                break;
+        }
+
+        sumB += tmpSumB;
+    });
+
+    return sumB;
 }
