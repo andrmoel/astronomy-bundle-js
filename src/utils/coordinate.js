0775650690a6f09d5d2e0795813378326a78761b
Andreas Moeller <andrmoel@gmail.com>
2020-10-19 19:00:40 +0200

    fear(coordinate): transform rectangular to spherical and vice versa

---
+++ src/utils/coordinate.js
@@ -0,0 +1,25 @@
+import {deg2rad, normalizeAngle, rad2deg} from './angle';
+
+export function rectangular2spherical(x, y, z) {
+    // Meeus 33.2
+    const lonRad = Math.atan2(y, x);
+    const lon = normalizeAngle(rad2deg(lonRad));
+
+    const latRad = Math.atan(z / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+    const lat = rad2deg(latRad);
+
+    const radiusVector = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+
+    return {lon, lat, radiusVector}
+}
+
+export function spherical2rectangular(lon, lat, radiusVector) {
+    const lonRad = deg2rad(lon);
+    const latRad = deg2rad(lat);
+
+    const x = radiusVector * Math.cos(latRad) * Math.cos(lonRad);
+    const y = radiusVector * Math.cos(latRad) * Math.sin(lonRad);
+    const z = radiusVector * Math.sin(latRad);
+
+    return {x, y, z};
+}
---
+++ src/utils/coordinate.test.js
@@ -0,0 +1,26 @@
+import {rectangular2spherical, spherical2rectangular} from './coordinate';
+import {round} from './math';
+
+it('tests rectangular2spherical', () => {
+    const x = 0.621746;
+    const y = -0.66481;
+    const z = -0.033134;
+
+    const spherical = rectangular2spherical(x, y, z);
+
+    expect(round(spherical.lon, 6)).toBe(313.082894);
+    expect(round(spherical.lat, 6)).toBe(-2.084721);
+    expect(round(spherical.radiusVector, 6)).toBe(0.910845);
+});
+
+it('tests spherical2rectangular', () => {
+    const lon = 313.082894;
+    const lat = -2.084721;
+    const radiusVector = 0.910845;
+
+    const rectangular = spherical2rectangular(lon, lat, radiusVector);
+
+    expect(round(rectangular.x, 6)).toBe(0.621746);
+    expect(round(rectangular.y, 6)).toBe(-0.66481);
+    expect(round(rectangular.z, 6)).toBe(-0.033134);
+});
