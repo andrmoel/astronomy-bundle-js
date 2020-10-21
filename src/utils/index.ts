9098e34d6ca63fd4839121aba3c20a1c0c6d5197
Andreas Moeller <andrmoel@gmail.com>
2020-10-21 14:22:47 +0200

    fkdpfkdspfk

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,8 @@
-export * from './angleCalc';
-export * from './coordinateCalc';
+import * as angleCalc from './angleCalc';
+import * as coordinateCalc from './coordinateCalc';
+import * as earthCalc from './earthCalc';
+import * as moonCalc from './moonCalc';
+import * as sunCalc from './sunCalc';
+import * as timeCalc from './timeCalc';
+
+export {angleCalc, coordinateCalc, earthCalc, moonCalc, sunCalc, timeCalc};
--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./angleCalc"), exports);
-__exportStar(require("./coordinateCalc"), exports);
+exports.timeCalc = exports.sunCalc = exports.moonCalc = exports.earthCalc = exports.coordinateCalc = exports.angleCalc = void 0;
+var angleCalc = require("./angleCalc");
+exports.angleCalc = angleCalc;
+var coordinateCalc = require("./coordinateCalc");
+exports.coordinateCalc = coordinateCalc;
+var earthCalc = require("./earthCalc");
+exports.earthCalc = earthCalc;
+var moonCalc = require("./moonCalc");
+exports.moonCalc = moonCalc;
+var sunCalc = require("./sunCalc");
+exports.sunCalc = sunCalc;
+var timeCalc = require("./timeCalc");
+exports.timeCalc = timeCalc;
