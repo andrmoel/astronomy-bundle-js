801caa1647df750af1534ef50f0a2f3ed72f5bee
Andreas Moeller <andrmoel@gmail.com>
2020-10-21 08:45:05 +0200

    feat(time): add more time functions

--- src/utils/time.test.js
+++ src/utils/time.test.js
@@ -1,14 +1,101 @@
 import {
-    getGreenwichMeanSiderealTime,
+    dayOfYear2time,
+    getDayOfWeek,
+    getDayOfYear,
+    isLeapYear,
     julianCenturiesJ20002julianDay,
     julianDay2julianCenturiesJ2000,
     julianDay2julianDay0,
     julianDay2julianMillenniaJ2000,
     julianDay2ModifiedJulianDay,
+    julianDay2time,
     julianMillenniaJ20002julianDay,
+    time2julianDay,
 } from './time';
 import {round} from './math';
-import {deg2time} from './angle';
+import {DAY_OF_WEEK_FRIDAY, DAY_OF_WEEK_WEDNESDAY} from '../constants/dayOfWeek';
+
+describe('test for time2julianDay', () => {
+    it('has valid times', () => {
+        expect(
+            time2julianDay({year: 2000, month: 1, day: 1, hour: 12, min: 0, sec: 0}),
+        ).toBe(2451545.0);
+        expect(
+            time2julianDay({year: 1999, month: 1, day: 1, hour: 0, min: 0, sec: 0}),
+        ).toBe(2451179.5);
+        expect(
+            time2julianDay({year: 1987, month: 1, day: 27, hour: 0, min: 0, sec: 0}),
+        ).toBe(2446822.5);
+        expect(
+            time2julianDay({year: 1987, month: 6, day: 19, hour: 12, min: 0, sec: 0}),
+        ).toBe(2446966.0);
+        expect(
+            time2julianDay({year: 1988, month: 1, day: 27, hour: 0, min: 0, sec: 0}),
+        ).toBe(2447187.5);
+        expect(
+            time2julianDay({year: 1988, month: 6, day: 19, hour: 12, min: 0, sec: 0}),
+        ).toBe(2447332.0);
+        expect(
+            time2julianDay({year: 1900, month: 1, day: 1, hour: 0, min: 0, sec: 0}),
+        ).toBe(2415020.5);
+        expect(
+            time2julianDay({year: 1600, month: 1, day: 1, hour: 0, min: 0, sec: 0}),
+        ).toBe(2305447.5);
+        expect(
+            time2julianDay({year: 1600, month: 12, day: 31, hour: 0, min: 0, sec: 0}),
+        ).toBe(2305812.5);
+        expect(
+            round(time2julianDay({year: 837, month: 4, day: 10, hour: 8, min: 0, sec: 0}), 6),
+        ).toBe(2026871.833333);
+        expect(
+            time2julianDay({year: -123, month: 12, day: 31, hour: 0, min: 0, sec: 0}),
+        ).toBe(1676496.5);
+        expect(
+            time2julianDay({year: -122, month: 1, day: 1, hour: 0, min: 0, sec: 0}),
+        ).toBe(1676497.5);
+        expect(
+            time2julianDay({year: -1000, month: 7, day: 12, hour: 12, min: 0, sec: 0}),
+        ).toBe(1356001.0);
+        expect(
+            time2julianDay({year: -1000, month: 2, day: 29, hour: 0, min: 0, sec: 0}),
+        ).toBe(1355866.5);
+        expect(
+            round(time2julianDay({year: -1001, month: 8, day: 17, hour: 21, min: 30, sec: 0}), 6),
+        ).toBe(1355671.395833);
+        expect(
+            time2julianDay({year: -4712, month: 1, day: 1, hour: 12, min: 0, sec: 0}),
+        ).toBe(0.0);
+    });
+
+    it('has an invalid time', () => {
+        try {
+            time2julianDay({year: 1582, month: 10, day: 10, hour: 12, min: 0, sec: 0});
+
+            fail('Expected error was not thrown');
+        } catch (error) {
+            expect(error.message).toBe('Date between 1582-10-04 and 1582-10-15 is not defined.');
+        }
+    });
+});
+
+it('tests for julianDay2time', () => {
+    expect(julianDay2time(2451545.0)).toEqual({year: 2000, month: 1, day: 1, hour: 12, min: 0, sec: 0});
+    expect(julianDay2time(2451179.5)).toEqual({year: 1999, month: 1, day: 1, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(2446822.5)).toEqual({year: 1987, month: 1, day: 27, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(2446966.0)).toEqual({year: 1987, month: 6, day: 19, hour: 12, min: 0, sec: 0});
+    expect(julianDay2time(2447187.5)).toEqual({year: 1988, month: 1, day: 27, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(2447332.0)).toEqual({year: 1988, month: 6, day: 19, hour: 12, min: 0, sec: 0});
+    expect(julianDay2time(2415020.5)).toEqual({year: 1900, month: 1, day: 1, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(2305447.5)).toEqual({year: 1600, month: 1, day: 1, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(2305812.5)).toEqual({year: 1600, month: 12, day: 31, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(2026871.833334)).toEqual({year: 837, month: 4, day: 10, hour: 8, min: 0, sec: 0});
+    expect(julianDay2time(1676496.5)).toEqual({year: -123, month: 12, day: 31, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(1676497.5)).toEqual({year: -122, month: 1, day: 1, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(1356001.0)).toEqual({year: -1000, month: 7, day: 12, hour: 12, min: 0, sec: 0});
+    expect(julianDay2time(1355866.5)).toEqual({year: -1000, month: 2, day: 29, hour: 0, min: 0, sec: 0});
+    expect(julianDay2time(1355671.395834)).toEqual({year: -1001, month: 8, day: 17, hour: 21, min: 30, sec: 0});
+    expect(julianDay2time(0.0)).toEqual({year: -4712, month: 1, day: 1, hour: 12, min: 0, sec: 0});
+});
 
 it('tests julianDay2julianDay0', () => {
     expect(julianDay2julianDay0(2451545.0)).toBe(2451544.5);
@@ -38,14 +125,53 @@ it('tests julianMillenniaJ20002julianDay', () => {
     expect(round(julianMillenniaJ20002julianDay(-0.012729637235), 1)).toBe(2446895.5);
 });
 
-it('test for getGreenwichMeanSiderealTime', () => {
-    // 1987-04-10 00:00:00
-    let GMST = getGreenwichMeanSiderealTime(-0.127296372348);
+it('tests dayOfYear2time', () => {
+    expect(dayOfYear2time(2000, 1)).toEqual({year: 2000, month: 1, day: 1, hour: 0, min: 0, sec: 0});
+    expect(dayOfYear2time(2000, 1.5)).toEqual({year: 2000, month: 1, day: 1, hour: 12, min: 0, sec: 0});
+    expect(dayOfYear2time(2000, 1.87654)).toEqual({year: 2000, month: 1, day: 1, hour: 21, min: 2, sec: 13});
+    expect(dayOfYear2time(2000, 10)).toEqual({year: 2000, month: 1, day: 10, hour: 0, min: 0, sec: 0});
+    expect(dayOfYear2time(2000, 10.5)).toEqual({year: 2000, month: 1, day: 10, hour: 12, min: 0, sec: 0});
+    expect(dayOfYear2time(2000, 10.87654)).toEqual({year: 2000, month: 1, day: 10, hour: 21, min: 2, sec: 13});
+    expect(dayOfYear2time(2000, 100)).toEqual({year: 2000, month: 4, day: 9, hour: 0, min: 0, sec: 0});
+    expect(dayOfYear2time(2011, 100)).toEqual({year: 2011, month: 4, day: 10, hour: 0, min: 0, sec: 0});
+    expect(dayOfYear2time(2011, 100.5)).toEqual({year: 2011, month: 4, day: 10, hour: 12, min: 0, sec: 0});
+    expect(dayOfYear2time(2011, 100.87654)).toEqual({year: 2011, month: 4, day: 10, hour: 21, min: 2, sec: 13});
+    expect(dayOfYear2time(2011, 321)).toEqual({year: 2011, month: 11, day: 17, hour: 0, min: 0, sec: 0});
+    expect(dayOfYear2time(2011, 321.5)).toEqual({year: 2011, month: 11, day: 17, hour: 12, min: 0, sec: 0});
+    expect(dayOfYear2time(2011, 321.87654)).toEqual({year: 2011, month: 11, day: 17, hour: 21, min: 2, sec: 13});
+});
 
-    expect(deg2time(GMST)).toBe('13h10m46.366s');
+it('tests getDayOfYear', () => {
+    expect(getDayOfYear({year: 2001, month: 1, day: 1, hour: 0, min: 0, sec: 0})).toBe(1);
+    expect(getDayOfYear({year: 2001, month: 1, day: 2, hour: 0, min: 0, sec: 0})).toBe(2);
+    expect(getDayOfYear({year: 2001, month: 1, day: 31, hour: 0, min: 0, sec: 0})).toBe(31);
+    expect(getDayOfYear({year: 2001, month: 2, day: 1, hour: 0, min: 0, sec: 0})).toBe(32);
+    expect(getDayOfYear({year: 2000, month: 10, day: 10, hour: 0, min: 0, sec: 0})).toBe(284);
+    expect(getDayOfYear({year: 2001, month: 10, day: 10, hour: 0, min: 0, sec: 0})).toBe(283);
+});
 
-    // 1987-04-10 19:21:00
-    GMST = getGreenwichMeanSiderealTime(-0.12727429842574);
+it('tests getDayOfWeek', () => {
+    expect(getDayOfWeek({year: 1954, month: 6, day: 30, hour: 0, min: 0, sec: 0})).toBe(DAY_OF_WEEK_WEDNESDAY);
+    expect(getDayOfWeek({year: 1954, month: 6, day: 30, hour: 7, min: 11, sec: 60})).toBe(DAY_OF_WEEK_WEDNESDAY);
+    expect(getDayOfWeek({year: 2019, month: 5, day: 31, hour: 0, min: 0, sec: 0})).toBe(DAY_OF_WEEK_FRIDAY);
+});
 
-    expect(deg2time(GMST)).toBe('8h34m57.09s');
+it('tests isLeapYear', () => {
+    expect(isLeapYear(1700)).toBeFalsy();
+    expect(isLeapYear(1800)).toBeFalsy();
+    expect(isLeapYear(1900)).toBeFalsy();
+    expect(isLeapYear(2000)).toBeTruthy();
+    expect(isLeapYear(2001)).toBeFalsy();
+    expect(isLeapYear(2002)).toBeFalsy();
+    expect(isLeapYear(2003)).toBeFalsy();
+    expect(isLeapYear(2004)).toBeTruthy();
+    expect(isLeapYear(2005)).toBeFalsy();
+    expect(isLeapYear(2006)).toBeFalsy();
+    expect(isLeapYear(2007)).toBeFalsy();
+    expect(isLeapYear(2008)).toBeTruthy();
+    expect(isLeapYear(2012)).toBeTruthy();
+    expect(isLeapYear(2016)).toBeTruthy();
+    expect(isLeapYear(2020)).toBeTruthy();
+    expect(isLeapYear(2040)).toBeTruthy();
+    expect(isLeapYear(2048)).toBeTruthy();
 });
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,4 +1,70 @@
-import {normalizeAngle} from './angle';
+import TimeInterface from '../time/interfaces/TimeInterface';
+import {round} from "./math";
+
+export function time2julianDay(time: TimeInterface): number {
+    const tmpYear = parseFloat(time.year + '.' + getDayOfYear(time));
+
+    let Y;
+    let M;
+    if (time.month > 2) {
+        Y = time.year;
+        M = time.month;
+    } else {
+        Y = time.year - 1;
+        M = time.month + 12;
+    }
+
+    const D = time.day;
+    const H = time.hour / 24 + time.min / 1440 + time.sec / 86400;
+
+    let A;
+    let B;
+    if (tmpYear >= 1582.288) { // YYYY-MM-DD >= 1582-10-15
+        A = Math.floor(Y / 100);
+        B = 2 - A + Math.floor(A / 4);
+    } else if (tmpYear <= 1582.277) { // YY-MM-DD <= 1582-10-04
+        B = 0;
+    } else {
+        throw new Error('Date between 1582-10-04 and 1582-10-15 is not defined.');
+    }
+
+    // Meeus 7.1
+    return Math.floor(365.25 * (Y + 4716)) + Math.floor(30.6001 * (M + 1)) + D + H + B - 1524.5;
+}
+
+export function julianDay2time(jd: number): TimeInterface {
+    jd = jd + 0.5;
+
+    const Z = Math.floor(jd);
+    const F = jd - Z;
+
+    let A = Z;
+    if (Z >= 2299161) {
+        const a = Math.floor((Z - 1867216.25) / 36524.25);
+        A = Z + 1 + a - Math.floor(a / 4);
+    }
+
+    const B = A + 1524;
+    const C = Math.floor((B - 122.1) / 365.25);
+    const D = Math.floor(365.25 * C);
+    const E = Math.floor((B - D) / 30.6001);
+
+    const dayOnMonth = B - D - Math.floor(30.6001 * E) + F;
+    const month = E < 14 ? E - 1 : E - 13;
+    const year = month > 2 ? C - 4716 : C - 4715;
+    const hour = (dayOnMonth - Math.floor(dayOnMonth)) * 24;
+    const min = (hour - Math.floor(hour)) * 60;
+    const sec = (min - Math.floor(min)) * 60;
+
+    return {
+        year: Math.floor(year),
+        month: Math.floor(month),
+        day: Math.floor(dayOnMonth),
+        hour: Math.floor(hour),
+        min: Math.floor(min),
+        sec: Math.floor(sec),
+    };
+}
 
 export function julianDay2julianDay0(jd: number): number {
     return Math.floor(jd + 0.5) - 0.5;
@@ -28,14 +94,45 @@ export function julianMillenniaJ20002julianDay(t: number): number {
     return julianCenturiesJ20002julianDay(T);
 }
 
-export function getGreenwichMeanSiderealTime(T: number): number {
-    const jd = julianCenturiesJ20002julianDay(T);
+export function dayOfYear2time(year: number, dayOfYear: number): TimeInterface {
+    // Meeus 7
+    const K = isLeapYear(year) ? 1 : 2;
+    const month = dayOfYear < 32 ? 1 : Math.floor((9 * (K + dayOfYear)) / 275 + 0.98);
+    const day = Math.floor(dayOfYear - Math.floor((275 * month) / 9) + K * Math.floor((month + 9) / 12) + 30);
 
-    // Meeus 12.4
-    const GMST = 280.46061837
-        + 360.98564736629 * (jd - 2451545)
-        + 0.000387933 * Math.pow(T, 2)
-        + Math.pow(T, 3) / 38710000;
+    const hourFloat = 24 * (dayOfYear - Math.floor(dayOfYear));
+    const hour = Math.floor(hourFloat);
+    const minFloat = 60 * (hourFloat - hour);
+    const min = Math.floor(minFloat);
+    const sec = round(60 * (minFloat - min));
 
-    return normalizeAngle(GMST);
+    return {year, month, day, hour, min, sec};
+}
+
+export function getDayOfYear(time: TimeInterface): number {
+    const K = isLeapYear(time.year) ? 1 : 2;
+    const M = time.month;
+    const D = time.day;
+
+    // Meeus 7.f
+    return Math.floor((275 * M) / 9) - K * Math.floor((M + 9) / 12) + D - 30;
+}
+
+export function getDayOfWeek(time: TimeInterface): number {
+    const jd = time2julianDay(time);
+
+    // Meeus 7.e
+    return Math.floor((jd + 1.5) % 7);
+}
+
+export function isLeapYear(year: number): boolean {
+    if (year / 4 !== Math.floor(year / 4)) {
+        return false;
+    } else if (year / 100 !== Math.floor(year / 100)) {
+        return true;
+    } else if (year / 400 !== Math.floor(year / 400)) {
+        return false;
+    } else {
+        return true;
+    }
 }
