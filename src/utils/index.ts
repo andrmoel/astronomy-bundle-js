e3ac1371f0a12093af00e885895c0363e8595a11
Andreas Moeller <andrmoel@gmail.com>
2020-10-21 14:15:29 +0200

    fkdpfkdspfk

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,2 @@
-import * as angleCalc from './angleCalc';
-import * as coordinateCalc from './coordinateCalc';
-import * as earthCalc from './earthCalc';
-import * as moonCalc from './moonCalc';
-import * as sunCalc from './sunCalc';
-import * as timeCalc from './timeCalc';
-
-export {angleCalc, coordinateCalc, earthCalc, moonCalc, sunCalc, timeCalc};
+export * from './angleCalc';
+export * from './coordinateCalc';
