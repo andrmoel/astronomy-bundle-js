ff858b04a9c75586b850f85be0f327ba0330291a
Andreas Möller <35029021+andrmoel@users.noreply.github.com>
2020-10-21 19:42:11 +0200

    feat(time-of-interest): add time of interest

---
+++ src/time/TimeOfInterest.ts
@@ -0,0 +1,6 @@
+import Time from './interfaces/Time';
+
+export default class TimeOfInterest {
+    constructor(public time: Time) {
+    }
+}
---
+++ src/time/createTimeOfInterest.ts
@@ -0,0 +1,56 @@
+import {timeCalc} from "../utils";
+import TimeOfInterest from './TimeOfInterest';
+
+export function fromTime(
+    year: number,
+    month: number,
+    day: number,
+    hour: number,
+    min: number,
+    sec: number
+): TimeOfInterest {
+    return new TimeOfInterest({year, month, day, hour, min, sec});
+}
+
+export function fromDate(date: Date): TimeOfInterest {
+    return new TimeOfInterest({
+        year: date.getUTCFullYear(),
+        month: date.getUTCMonth() + 1,
+        day: date.getUTCDate(),
+        hour: date.getUTCHours(),
+        min: date.getUTCMinutes(),
+        sec: date.getUTCSeconds(),
+    });
+}
+
+export function fromCurrentTime() {
+    const date = new Date(Date.now());
+
+    return new TimeOfInterest({
+        year: date.getUTCFullYear(),
+        month: date.getUTCMonth() + 1,
+        day: date.getUTCDate(),
+        hour: date.getUTCHours(),
+        min: date.getUTCMinutes(),
+        sec: date.getUTCSeconds(),
+    });
+}
+
+export function fromYearOfDay(year: number, dayOfYear: number): TimeOfInterest {
+    const time = timeCalc.dayOfYear2time(year, dayOfYear);
+
+    return new TimeOfInterest(time);
+}
+
+export function fromJulianDay(jd: number): TimeOfInterest {
+    const time = timeCalc.julianDay2time(jd);
+
+    return new TimeOfInterest(time);
+}
+
+export function fromJulianCenturiesJ2000(T: number): TimeOfInterest {
+    const jd = timeCalc.julianCenturiesJ20002julianDay(T);
+    const time = timeCalc.julianDay2time(jd);
+
+    return new TimeOfInterest(time);
+}
