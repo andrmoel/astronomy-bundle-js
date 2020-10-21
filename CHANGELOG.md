74d95e6eac0f63cf8c7d2650efa157b2aec5c83a
github version update <noreply@github.com>
2020-10-21 09:13:18 +0000

    chore(release): 2.5.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### 2.5.7 (2020-10-21)
+
 ### 2.5.6 (2020-10-21)
 
 ### 2.5.5 (2020-10-21)
--- lib/utils/moon.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function getMeanElongation(T: number): number;
-export declare function getMeanAnomaly(T: number): number;
-export declare function getArgumentOfLatitude(T: number): number;
-export declare function getMeanLongitude(T: number): number;
-export declare function getEquatorialHorizontalParallax(T: number): number;
-export declare function getDistanceToEarth(T: number): number;
-export declare function getLongitude(T: number): number;
-export declare function getApparentLongitude(T: number): number;
-export declare function getLatitude(T: number): number;
