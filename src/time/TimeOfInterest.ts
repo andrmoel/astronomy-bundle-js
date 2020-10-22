c57e47a58ba82b3bc0ac3f8c68f5f68951f784ab
Andreas Moeller <andrmoel@gmail.com>
2020-10-22 08:23:44 +0200

    feat(moon): create moon object

--- src/time/TimeOfInterest.ts
+++ src/time/TimeOfInterest.ts
@@ -1,6 +1,12 @@
-import Time from './interfaces/Time';
+import ITime from './interfaces/ITime';
+import {timeCalc} from '../utils';
 
 export default class TimeOfInterest {
-    constructor(public time: Time) {
+    public jd: number = 0.0;
+    public T: number = 0.0;
+
+    constructor(public time: ITime) {
+        this.jd = timeCalc.time2julianDay(time);
+        this.T = timeCalc.julianDay2julianCenturiesJ2000(this.jd);
     }
 }
