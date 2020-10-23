fc2911424cf32403b0e20a3885dda305b508c5ec
Andreas Moeller <andrmoel@gmail.com>
2020-10-23 17:20:09 +0200

    feat(coordinates): transform ecliptic to equatorial and vice versa

--- src/utils/coordinateCalc.test.js
+++ src/utils/coordinateCalc.test.js
@@ -1,4 +1,9 @@
-import {rectangular2spherical, spherical2rectangular} from './coordinateCalc';
+import {
+    eclipticSpherical2equatorialSpherical,
+    equatorialSpherical2eclipticSpherical,
+    rectangular2spherical,
+    spherical2rectangular,
+} from './coordinateCalc';
 import {round} from './math';
 
 it('tests rectangular2spherical', () => {
@@ -23,4 +28,30 @@ it('tests spherical2rectangular', () => {
     expect(round(rectangular.x, 6)).toBe(0.621746);
     expect(round(rectangular.y, 6)).toBe(-0.66481);
     expect(round(rectangular.z, 6)).toBe(-0.033134);
+});
+
+it('tests eclipticSpherical2equatorialSpherical', () => {
+    const T = -0.070321697467488; // 1992-12-20 00:00:00
+    const lon = 313.082894;
+    const lat = -2.084721;
+    const radiusVector = 0.910845;
+
+    const coords = eclipticSpherical2equatorialSpherical(lon, lat, radiusVector, T);
+
+    expect(round(coords.rightAscension, 6)).toBe(316.174262);
+    expect(round(coords.declination, 6)).toBe(-18.887468);
+    expect(round(coords.radiusVector, 6)).toBe(0.910845);
+});
+
+it('tests equatorialSpherical2eclipticSpherical', () => {
+    const T = -0.070321697467488; // 1992-12-20 00:00:00
+    const rightAscension = 316.174262;
+    const declination = -18.887468;
+    const radiusVector = 0.910845;
+
+    const coords = equatorialSpherical2eclipticSpherical(rightAscension, declination, radiusVector, T);
+
+    expect(round(coords.lon, 6)).toBe(313.082894);
+    expect(round(coords.lat, 6)).toBe(-2.084721);
+    expect(round(coords.radiusVector, 6)).toBe(0.910845);
 });
--- src/utils/coordinateCalc.ts
+++ src/utils/coordinateCalc.ts
@@ -1,10 +1,10 @@
+import IEquatorialSphericalCoordinates from '../coordinates/interfaces/IEquatorialSphericalCoordinates';
+import IEclipticSphericalCoordinates from '../coordinates/interfaces/IEclipticSphericalCoordinates';
+import IRectangularCoordinates from '../coordinates/interfaces/IRectangularCoordinates';
 import {deg2rad, normalizeAngle, rad2deg} from './angleCalc';
+import {earthCalc} from './index';
 
-export function rectangular2spherical(x: number, y: number, z: number): {
-    lon: number,
-    lat: number,
-    radiusVector: number
-} {
+export function rectangular2spherical(x: number, y: number, z: number): IEclipticSphericalCoordinates {
     // Meeus 33.2
     const lonRad = Math.atan2(y, x);
     const lon = normalizeAngle(rad2deg(lonRad));
@@ -17,11 +17,7 @@ export function rectangular2spherical(x: number, y: number, z: number): {
     return {lon, lat, radiusVector};
 }
 
-export function spherical2rectangular(lon: number, lat: number, radiusVector: number): {
-    x: number,
-    y: number,
-    z: number
-} {
+export function spherical2rectangular(lon: number, lat: number, radiusVector: number): IRectangularCoordinates {
     const lonRad = deg2rad(lon);
     const latRad = deg2rad(lat);
 
@@ -30,4 +26,56 @@ export function spherical2rectangular(lon: number, lat: number, radiusVector: nu
     const z = radiusVector * Math.sin(latRad);
 
     return {x, y, z};
+}
+
+export function eclipticSpherical2equatorialSpherical(
+    lon: number,
+    lat: number,
+    radiusVector: number,
+    T: number,
+): IEquatorialSphericalCoordinates {
+    const eps = earthCalc.getTrueObliquityOfEcliptic(T);
+    const epsRad = deg2rad(eps);
+    const lonRad = deg2rad(lon);
+    const latRad = deg2rad(lat);
+
+    // Meeus 13.3
+    const n = Math.sin(lonRad) * Math.cos(epsRad) - (Math.sin(latRad) / Math.cos(latRad)) * Math.sin(epsRad);
+    const d = Math.cos(lonRad);
+    const rightAscensionRad = Math.atan2(n, d);
+    const rightAscension = normalizeAngle(rad2deg(rightAscensionRad));
+
+    // Meeus 13.4
+    const declinationRad = Math.asin(
+        Math.sin(latRad) * Math.cos(epsRad) + Math.cos(latRad) * Math.sin(epsRad) * Math.sin(lonRad)
+    );
+    const declination = rad2deg(declinationRad);
+
+    return {rightAscension, declination, radiusVector};
+}
+
+export function equatorialSpherical2eclipticSpherical(
+    rightAscension: number,
+    declination: number,
+    radiusVector: number,
+    T: number,
+): IEclipticSphericalCoordinates {
+    const eps = earthCalc.getTrueObliquityOfEcliptic(T);
+    const epsRad = deg2rad(eps);
+    const rightAscensionRad = deg2rad(rightAscension);
+    const declinationRad = deg2rad(declination);
+
+    // Meeus 13.1
+    const n = Math.sin(rightAscensionRad) * Math.cos(epsRad) + Math.tan(declinationRad) * Math.sin(epsRad);
+    const d = Math.cos(rightAscensionRad);
+    const lonRad = Math.atan2(n, d);
+    const lon = normalizeAngle(rad2deg(lonRad));
+
+    // Meeus 13.2
+    const latRad = Math.asin(
+        Math.sin(declinationRad) * Math.cos(epsRad) - Math.cos(declinationRad) * Math.sin(epsRad) * Math.sin(rightAscensionRad)
+    );
+    const lat = rad2deg(latRad);
+
+    return {lon, lat, radiusVector};
 }
