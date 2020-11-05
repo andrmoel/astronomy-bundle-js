67a62e106f45b5d30d9288e01f3072e84dcc195c
Andreas Möller <35029021+andrmoel@users.noreply.github.com>
2020-11-05 18:38:29 +0100

    chore(planets): cache VSOP87 calculations

---
+++ src/planets/calculations/vsop87Calc.ts
@@ -0,0 +1,25 @@
+export function calculateVSOP87(terms: Array<Array<Array<number>>>, t: number): number {
+    let result = 0.0;
+
+    terms.forEach(
+        (term: Array<Array<number>>, key: number) => {
+            result += _sumUpTerm(term, t) * Math.pow(t, key)
+        }
+    );
+
+    return result;
+}
+
+function _sumUpTerm(terms: Array<Array<number>>, t: number): number {
+    let result = 0.0;
+
+    terms.forEach((term: Array<number>) => {
+        const a = term[0] || 0;
+        const b = term[1] || 0;
+        const c = term[2] || 0;
+
+        result += a * Math.cos(b + c * t)
+    });
+
+    return result;
+}
