bf5c3129905a5be4af546a47f16c8585f783314a
Andreas Moeller <andrmoel@gmail.com>
2020-11-11 22:18:09 +0100

    chore(math): add interpolation for 5

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,7 +4,7 @@ export function round(value: number, decimals = 0): number {
     return Math.round(value * p) / p;
 }
 
-export function pad(num: number|string, size: number): string {
+export function pad(num: number | string, size: number): string {
     let numStr = num.toString();
 
     while (numStr.length < size) {
@@ -12,4 +12,33 @@ export function pad(num: number|string, size: number): string {
     }
 
     return numStr;
+}
+
+export function tabularInterpolation5(values: Array<number>): number {
+    const y3 = values[2];
+    const A = values[1] - values[0];
+    const B = values[2] - values[1];
+    const C = values[3] - values[2];
+    const D = values[4] - values[3];
+    const E = B - A;
+    const F = C - B;
+    const G = D - C;
+    const H = F - E;
+    const J = G - F;
+    const K = J - H;
+
+    let n0 = 0.0;
+
+    while (true) {
+        // Meeus 3.10
+        const numerator = -24 * y3 + Math.pow(n0, 2) * (K - 12 * F) - 2 * Math.pow(n0, 3) * (H + J) - Math.pow(n0, 4) * K;
+        const denominator = 2 * (6 * B + 6 * C - H - J);
+        const n0New = numerator / denominator;
+
+        if (n0New === n0) {
+            return n0;
+        }
+
+        n0 = n0New;
+    }
 }
