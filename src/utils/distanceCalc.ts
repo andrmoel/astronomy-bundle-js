57708ff297b0f58e01c0862876f20dc2616c8795
Andreas Moeller <andrmoel@gmail.com>
2021-02-07 07:42:44 +0100

    refactor

--- src/utils/distanceCalc.ts
+++ src/utils/distanceCalc.ts
@@ -1,6 +1,3 @@
-import {EquatorialSphericalCoordinates} from '../coordinates/coordinateTypes';
-import {deg2rad, rad2deg} from './angleCalc';
-
 const AU_UNIT_OF_LENGTH = 149597870700.0;
 
 export function au2km(R: number): number {
@@ -9,21 +6,4 @@ export function au2km(R: number): number {
 
 export function km2au(km: number): number {
     return km / (AU_UNIT_OF_LENGTH / 1000);
-}
-
-export function getAngularSeparation(
-    coords1: EquatorialSphericalCoordinates,
-    coords2: EquatorialSphericalCoordinates,
-): number {
-    const raRad1 = deg2rad(coords1.rightAscension);
-    const dRad1 = deg2rad(coords1.declination);
-    const raRad2 = deg2rad(coords2.rightAscension);
-    const dRad2 = deg2rad(coords2.declination);
-
-    const dRad = Math.acos(
-        Math.sin(dRad1) * Math.sin(dRad2)
-        + Math.cos(dRad1) * Math.cos(dRad2) * Math.cos(raRad1 - raRad2)
-    );
-
-    return rad2deg(dRad);
 }
--- src/utils/observationCalc.ts
+++ src/utils/observationCalc.ts
@@ -69,3 +69,20 @@ export function getAngularDiameter(distance: number, trueDiameter: number): numb
 
     return rad2deg(delta);
 }
+
+export function getAngularSeparation(
+    coords1: EquatorialSphericalCoordinates,
+    coords2: EquatorialSphericalCoordinates,
+): number {
+    const raRad1 = deg2rad(coords1.rightAscension);
+    const dRad1 = deg2rad(coords1.declination);
+    const raRad2 = deg2rad(coords2.rightAscension);
+    const dRad2 = deg2rad(coords2.declination);
+
+    const dRad = Math.acos(
+        Math.sin(dRad1) * Math.sin(dRad2)
+        + Math.cos(dRad1) * Math.cos(dRad2) * Math.cos(raRad1 - raRad2)
+    );
+
+    return rad2deg(dRad);
+}
