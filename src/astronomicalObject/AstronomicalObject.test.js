5ad1a305909c9fe4ad62cb6d79978c77a0aa9405
Andreas Moeller <andrmoel@gmail.com>
2021-04-20 09:29:36 +0200

    refactor(astronomical-object): make name property immutable
    BREAKING CHANGE: method getName() was removed from AstronomicalObject as is replaced by "name"

--- src/astronomicalObject/AstronomicalObject.test.js
+++ src/astronomicalObject/AstronomicalObject.test.js
@@ -1,8 +1,6 @@
 import {createTimeOfInterest} from '../time';
 import AstronomicalObject from './AstronomicalObject';
 
-jest.spyOn(global.Date, 'now').mockReturnValueOnce('2020-10-21 10:00:00Z');
-
 class TestClass extends AstronomicalObject {
     constructor(toi) {
         super('TestClass', toi);
@@ -10,9 +8,7 @@ class TestClass extends AstronomicalObject {
 }
 
 it('tests get name', () => {
-    const toi = createTimeOfInterest.fromTime(2000, 5, 10, 0, 0, 0);
-
-    const astronomicalObject = new TestClass(toi);
+    const astronomicalObject = new TestClass();
 
     expect(astronomicalObject.name).toBe('TestClass');
 });
@@ -26,6 +22,8 @@ it('creates an AstronomicalObject with TimeOfInterest', () => {
 });
 
 it('creates an AstronomicalObject without TimeOfInterest', () => {
+    jest.spyOn(global.Date, 'now').mockReturnValueOnce('2020-10-21 10:00:00Z');
+
     const astronomicalObject = new TestClass();
 
     expect(astronomicalObject.toi.time).toEqual({year: 2020, month: 10, day: 21, hour: 10, min: 0, sec: 0});
