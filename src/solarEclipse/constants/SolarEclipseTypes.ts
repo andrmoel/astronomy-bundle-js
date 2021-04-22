2282a79a2efc6e71d6006f9802b5685e11f83e5f
Andreas Moeller <andrmoel@gmail.com>
2021-04-22 14:30:21 +0200

    refactor(constants): move more moon and sun constants to aggregate folders

---
+++ src/solarEclipse/constants/SolarEclipseTypes.ts
@@ -0,0 +1,4 @@
+export const SOLAR_ECLIPSE_TYPE_NONE = 'none';
+export const SOLAR_ECLIPSE_TYPE_PARTIAL = 'partial';
+export const SOLAR_ECLIPSE_TYPE_ANNULAR = 'annular';
+export const SOLAR_ECLIPSE_TYPE_TOTAL = 'total';
