b2757bca6b9bc35307a5af824182bef3960ac8bc
Andreas Moeller <andrmoel@gmail.com>
2021-04-25 00:17:05 +0200

    chore(solar-eclipse): validate if eclipse exists in factory

--- src/solarEclipse/createSolarEclipse.test.js
+++ src/solarEclipse/createSolarEclipse.test.js
@@ -2,15 +2,28 @@ import {createTimeOfInterest} from '../time';
 import createSolarEclipse from './createSolarEclipse';
 import SolarEclipse from './SolarEclipse';
 
-it('tests createSolarEclipse', () => {
-    const toi = createTimeOfInterest.fromCurrentTime();
-    const solarEclipse = createSolarEclipse(toi);
-
-    expect(solarEclipse).toBeInstanceOf(SolarEclipse);
+it('tests createSolarEclipse without TOI', () => {
+    try {
+        createSolarEclipse();
+    } catch (error) {
+        expect(error.message).toBe('Could not find Solar Eclipse for given date: undefined');
+    }
 });
 
-it('tests createSolarEclipse without TOI', () => {
-    const solarEclipse = createSolarEclipse();
+it('tests createSolarEclipse with invalid TOI', () => {
+    const toi = createTimeOfInterest.fromTime(2020, 1, 1, 0, 0, 0);
+
+    try {
+        createSolarEclipse(toi);
+    } catch (error) {
+        expect(error.message).toBe('Could not find Solar Eclipse for given date: 2020-01-01 00:00:00');
+    }
+});
+
+it('tests createSolarEclipse with valid TOI', () => {
+    const toi = createTimeOfInterest.fromTime(2020, 12, 14, 0, 0, 0);
+
+    const solarEclipse = createSolarEclipse(toi);
 
     expect(solarEclipse).toBeInstanceOf(SolarEclipse);
 });
--- src/solarEclipse/solarEclipseExists.test.js
+++ src/solarEclipse/solarEclipseExists.test.js
@@ -1,6 +1,16 @@
 import {createTimeOfInterest} from '../time';
 import solarEclipseExists from './solarEclipseExists';
 
+it('has no valid TOI', () => {
+    expect(solarEclipseExists()).toBeFalsy();
+});
+
+it('has no solar eclipse', () => {
+    const toi = createTimeOfInterest.fromTime(2017, 8, 22, 0, 0, 0);
+
+    expect(solarEclipseExists(toi)).toBeFalsy();
+});
+
 it('has a solar eclipse with date at 00:00', () => {
     const toi = createTimeOfInterest.fromTime(2017, 8, 21, 0, 0, 0);
 
@@ -11,10 +21,4 @@ it('has a solar eclipse with date and time', () => {
     const toi = createTimeOfInterest.fromTime(2017, 8, 21, 22, 54, 10);
 
     expect(solarEclipseExists(toi)).toBeTruthy();
-});
-
-it('has no solar eclipse', () => {
-    const toi = createTimeOfInterest.fromTime(2017, 8, 22, 0, 0, 0);
-
-    expect(solarEclipseExists(toi)).toBeFalsy();
 });
