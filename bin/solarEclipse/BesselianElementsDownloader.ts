9c7eaaa6295332c126162949bdb3f72fcd16bc9a
Andreas Möller <35029021+andrmoel@users.noreply.github.com>
2021-04-24 19:30:08 +0200

    feat(besselian-elements): add function to download besselian elements from NASA's website

---
+++ bin/solarEclipse/BesselianElementsDownloader.ts
@@ -0,0 +1,66 @@
+import {existsSync, writeFileSync} from 'fs';
+import axios from 'axios';
+import {decodeHTML} from 'entities';
+import {SOLAR_ECLIPSES} from '../../src/solarEclipse/constants/solarEclipseList';
+import {createTimeOfInterest} from '../../src/time';
+import {pad} from '../../src/utils/math';
+import BesselianElementsParser from './BesselianElementsParser';
+
+export default class BesselianElementsDownloader {
+    public async run(): Promise<void> {
+        for (let i = 0; i < SOLAR_ECLIPSES.length; i++) {
+            await BesselianElementsDownloader.downloadSingleBesselianElements(SOLAR_ECLIPSES[i]);
+        }
+    }
+
+    private static async downloadSingleBesselianElements(jd: number): Promise<void> {
+        const url = BesselianElementsDownloader.getDownloadUrl(jd);
+        const file = BesselianElementsDownloader.getLocalFile(jd);
+
+        await BesselianElementsDownloader.downloadFile(url, file);
+    }
+
+    private static getDownloadUrl(jd: number): string {
+        const {year, month, day} = createTimeOfInterest.fromJulianDay(jd).getTime();
+
+        const sign = year >= 0 ? '' : '-';
+        const dateString = sign + pad(Math.abs(year), 4) + pad(month, 2) + pad(day, 2);
+
+        return `https://eclipse.gsfc.nasa.gov/SEsearch/SEdata.php?Ecl=${dateString}`;
+    }
+
+    private static getLocalFile(jd: number): string {
+        const dir = __dirname + '/../../src/solarEclipse/resources/besselianElements/';
+        const fileName = jd + '.json';
+
+        return dir + fileName;
+    }
+
+    private static async downloadFile(url: string, file: string): Promise<void> {
+        if (!existsSync(file)) {
+            const {data} = await axios.get(url);
+
+            console.log('Downloaded besselian elements: ' + url);
+
+            const besselianElementsString = BesselianElementsDownloader.parseBesselianElementsString(data, url);
+
+            const parser = new BesselianElementsParser(besselianElementsString);
+            const besselianElements = parser.parseBesselianElements();
+
+            writeFileSync(file, JSON.stringify(besselianElements));
+        }
+    }
+
+    private static parseBesselianElementsString(content: string, url: string): string {
+        const regExp = new RegExp(/<pre>(.*?)<\/pre>/si);
+        const matches = content.match(regExp);
+
+        if (matches) {
+            const html = matches[1].replace(/(<([^>]+)>)/gi, '');
+
+            return decodeHTML(html);
+        }
+
+        throw Error(`Could not parse besselian elements string: ${url}`);
+    }
+}
---
+++ bin/solarEclipse/BesselianElementsParser.ts
@@ -0,0 +1,143 @@
+import {BesselianElementsTypes} from '../../src/solarEclipse/types/besselianElementsTypes';
+
+type Polynomials = {
+    x: Array<number>;
+    y: Array<number>;
+    d: Array<number>;
+    l1: Array<number>;
+    l2: Array<number>;
+    mu: Array<number>;
+}
+
+type TanF1F2 = {
+    tanF1: number,
+    tanF2: number,
+}
+
+export default class BesselianElementsParser {
+    private rows: string[] = [];
+
+    constructor(private content: string) {
+        this.rows = this.content.split('\n');
+    }
+
+    public parseBesselianElements(): BesselianElementsTypes {
+        const tMax = this.parseTMax();
+        const t0 = this.parseT0();
+        const dT = this.parseDeltaT();
+        const polynomials = this.parsePolynomials();
+        const tanF1F2 = this.parseTanF1F2();
+        const latGreatestEclipse = this.parseLatitudeGreatestEclipse();
+        const lonGreatestEclipse = this.parseLongitudeGreatestEclipse();
+
+        return {
+            tMax,
+            t0,
+            dT,
+            ...polynomials,
+            ...tanF1F2,
+            latGreatestEclipse,
+            lonGreatestEclipse,
+        };
+    }
+
+    private parseTMax(): number {
+        const regExp = new RegExp(/Instant of.*J[.]?D[.]? = ([0-9.]+)/si);
+        const matches = this.content.match(regExp);
+
+        if (matches) {
+            return parseFloat(matches[1]);
+        }
+
+        return 0.0;
+    }
+
+    private parseT0(): number {
+        const regExp = new RegExp(/t0 =.*?([0-9.]+).*?TDT/si);
+        const matches = this.content.match(regExp);
+
+        if (matches) {
+            return parseFloat(matches[1]);
+        }
+
+        return 0.0;
+    }
+
+    private parseDeltaT(): number {
+        const regExp = new RegExp(/ΔT =.*?([0-9.]+).*?s/si);
+        const matches = this.content.match(regExp);
+
+        if (matches) {
+            return parseFloat(matches[1]);
+        }
+
+        return 0.0;
+    }
+
+    private parsePolynomials(): Polynomials {
+        const polynomials: Polynomials = {
+            x: [],
+            y: [],
+            d: [],
+            l1: [],
+            l2: [],
+            mu: [],
+        };
+
+        const regExp = new RegExp(/[0-9]{1} +[0-9.-]+ +[0-9.-]+/si);
+
+        this.rows.forEach((row: string) => {
+            if (row.match(regExp)) {
+                const parts = row.split(/\s+/).filter((value) => value);
+
+                polynomials.x.push(parseFloat(parts[1]));
+                polynomials.y.push(parseFloat(parts[2]));
+                polynomials.d.push(parseFloat(parts[3]));
+                polynomials.l1.push(parseFloat(parts[4]));
+                polynomials.l2.push(parseFloat(parts[5]));
+                polynomials.mu.push(parseFloat(parts[6]));
+            }
+        });
+
+        return polynomials;
+    }
+
+    private parseTanF1F2(): TanF1F2 {
+        const regExp = new RegExp(/Tan [f|ƒ]1 = ([0-9.]+).*?Tan [f|ƒ]2 = ([0-9.]+)/si);
+        const matches = this.content.match(regExp);
+
+        if (matches) {
+            return {
+                tanF1: parseFloat(matches[1]),
+                tanF2: parseFloat(matches[2]),
+            }
+        }
+
+        return {
+            tanF1: 0.0,
+            tanF2: 0.0,
+        }
+    }
+
+    private parseLatitudeGreatestEclipse(): number {
+        const regExp = new RegExp(/Latitude:([0-9. ]+)° ([NS])/si);
+        const matches = this.content.match(regExp);
+
+        if (matches) {
+            return parseFloat(matches[1]) * (matches[2] === 'N' ? 1 : -1);
+        }
+
+        return 0.0;
+    }
+
+    private parseLongitudeGreatestEclipse(): number {
+        const regExp = new RegExp(/Longitude:([0-9. ]+)° ([WE])/si);
+        const matches = this.content.match(regExp);
+
+        if (matches) {
+            return parseFloat(matches[1]) * (matches[2] === 'E' ? 1 : -1);
+        }
+
+        return 0.0;
+    }
+}
