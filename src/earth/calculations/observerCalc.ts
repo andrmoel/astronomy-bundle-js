ea55cf1a12d39a9fef5504c8641ddc7ad8756700
Andreas Möller <andrmoel@gmail.com>
2021-05-11 01:36:04 +0200

    feat(circumstances): calculate circumstances

--- src/earth/calculations/observerCalc.ts
+++ src/earth/calculations/observerCalc.ts
@@ -1,14 +1,15 @@
 import {GeocentricPosition, Location} from '../types/LocationTypes';
 import {deg2rad} from '../../utils/angleCalc';
+import {EARTH_AXIS_RATIO, EARTH_RADIUS} from '../constants/dimensions';
 
 export function getGeocentricPosition(location: Location): GeocentricPosition {
     const {lat, elevation} = location;
     const latRad = deg2rad(lat);
 
-    const tmp = Math.atan(0.996647189335 * Math.tan(latRad));
+    const tmp = Math.atan(EARTH_AXIS_RATIO * Math.tan(latRad));
 
-    const rhoSin0 = (0.996647189335 * Math.sin(tmp)) + (elevation * Math.sin(latRad) / 6378137.0);
-    const rhoCos0 = Math.cos(tmp) + (elevation * Math.cos(latRad) / 6378137.0);
+    const rhoSin0 = (EARTH_AXIS_RATIO * Math.sin(tmp)) + (elevation * Math.sin(latRad) / 6378137.0);
+    const rhoCos0 = Math.cos(tmp) + (elevation * Math.cos(latRad) / EARTH_RADIUS);
 
     return {
         rhoSin0,
