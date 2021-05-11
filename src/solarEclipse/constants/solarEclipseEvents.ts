d1fbf2f34684fd377f9877d95b49d0b021a6de9a
Andreas Möller <andrmoel@gmail.com>
2021-05-11 09:42:37 +0200

    revert

--- src/solarEclipse/constants/solarEclipseEvents.ts
+++ src/solarEclipse/constants/solarEclipseEvents.ts
@@ -1,7 +1,5 @@
-export enum SolarEclipseEventType {
-    'c1',
-    'c2',
-    'mid',
-    'c3',
-    'c4',
-}
+export const SOLAR_ECLIPSE_EVENT_CONTACT_1 = 'c1';
+export const SOLAR_ECLIPSE_EVENT_CONTACT_2 = 'c2';
+export const SOLAR_ECLIPSE_EVENT_MAX = 'max';
+export const SOLAR_ECLIPSE_EVENT_CONTACT_3 = 'c3';
+export const SOLAR_ECLIPSE_EVENT_CONTACT_4 = 'c4';
