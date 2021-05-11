fc3934f0aff24d6b74402fb5d497cf7c851e7aa6
Andreas Möller <andrmoel@gmail.com>
2021-05-11 09:41:20 +0200

    revert

--- src/earth/types/LocationTypes.ts
+++ src/earth/types/LocationTypes.ts
@@ -1,10 +1,5 @@
 export type Location = {
     lat: number,
     lon: number,
-    elevation: number,
-}
-
-export type GeocentricPosition = {
-    rhoSin0: number,
-    rhoCos0: number,
+    elevation?: number,
 }
