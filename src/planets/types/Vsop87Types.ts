4daf9c8d9f6ac16107b2638044270ad720d77b62
Andreas Moeller <andrmoel@gmail.com>
2021-10-08 09:17:08 +0200

    refactor(typescript): use typescript everywhere

--- src/planets/types/Vsop87Types.ts
+++ src/planets/types/Vsop87Types.ts
@@ -1,5 +1,9 @@
+export type Vsop87Element = Array<Array<number>>;
+
+export type Vsop87Group = Array<Vsop87Element>;
+
 export type Vsop87 = {
-    VSOP87_X: Array<Array<Array<number>>>,
-    VSOP87_Y: Array<Array<Array<number>>>,
-    VSOP87_Z: Array<Array<Array<number>>>
+    VSOP87_X: Vsop87Group,
+    VSOP87_Y: Vsop87Group,
+    VSOP87_Z: Vsop87Group
 }
