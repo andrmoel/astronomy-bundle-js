f9902cc0f438ff5a9d3f49c0f4fbedf21a40e5e7
Andreas Moeller <andrmoel@gmail.com>
2021-12-27 14:33:02 +0100

    feat(solar-eclipse): calculate duration

---
+++ src/solarEclipse/LocalCircumstances.test.ts
@@ -0,0 +1,38 @@
+import {round} from '../utils/math';
+import Location from '../earth/Location';
+import LocalCircumstances from './LocalCircumstances';
+
+// TSE 2020-12-14
+const besselianElements = {
+    tMax: 2459198.177,
+    t0: 16,
+    dT: 72.1,
+    x: [-0.181824, 0.5633567, 0.0000216, -0.000009],
+    y: [-0.269645, -0.0858122, 0.0001884, 0.0000015],
+    d: [-23.2577591, -0.001986, 0.000006, 0],
+    l1: [0.543862, 0.000097, -0.0000126, 0],
+    l2: [-0.002265, 0.0000965, -0.0000125, 0],
+    mu: [61.265911, 14.9965, 0, 0],
+    tanF1: 0.0047502,
+    tanF2: 0.0047266,
+    latGreatestEclipse: -40.3,
+    lonGreatestEclipse: -67.9,
+}
+
+const location = new Location(-39.53940, -70.37216, 450);
+
+it('tests getDurationOfPartialEclipse', () => {
+    const localCircumstances = new LocalCircumstances(besselianElements, location);
+
+    const result = localCircumstances.getDurationOfPartialEclipse();
+
+    expect(round(result, 1)).toBe(10239.1);
+});
+
+it('tests getDurationOfCentralEclipse', () => {
+    const localCircumstances = new LocalCircumstances(besselianElements, location);
+
+    const result = localCircumstances.getDurationOfCentralEclipse();
+
+    expect(round(result, 1)).toBe(111.4);
+});
--- src/solarEclipse/LocalCircumstances.ts
+++ src/solarEclipse/LocalCircumstances.ts
@@ -1,7 +1,16 @@
 import Location from '../earth/Location';
 import {BesselianElements} from './types/besselianElementsTypes';
+import {getCentralDuration, getDuration} from './calculations/eclipseCalc';
 
 export default class LocalCircumstances {
     constructor(private besselianElements: BesselianElements, private location: Location) {
+    }
+
+    public getDurationOfPartialEclipse(): number {
+        return getDuration(this.besselianElements, this.location);
+    }
+
+    public getDurationOfCentralEclipse(): number {
+        return getCentralDuration(this.besselianElements, this.location)
     }
 }
