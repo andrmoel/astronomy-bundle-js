8541ef2073a2e1ed2831e2b521dbc09cd913eb94
Andreas Möller <andrmoel@gmail.com>
2026-06-12 09:09:10 +0200

    style(): fix code style

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,12 +1,11 @@
-export {default as Location} from '@package/location/models/Location';
-export {default as TimeOfInterest} from '@package/time/models/TimeOfInterest';
-
 // Constants
 export * from '@app/constants/earth';
 export * from '@app/constants/epoch';
 export * from '@app/constants/moon';
 export * from '@app/constants/units';
-
 // Types
 export * from '@app/types/CoordinateTypes';
 export * from '@app/types/LocationTypes';
+// Models
+export {default as Location} from '@package/location/models/Location';
+export {default as TimeOfInterest} from '@package/time/models/TimeOfInterest';
